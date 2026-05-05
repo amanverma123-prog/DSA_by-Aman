e17c083da01f7cc24b8b41fa8ce2842c0f933347
Aman Verma <amanverma3911@gmail.com>
2026-05-05 19:33:45 +0530

    Implemented LC - 290: Word Pattern

---
+++ Strings/wordPattern290.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+bool wordPattern(string pattern, string s) {
+    unordered_map<char, string> mp;
+    unordered_map<string, char> mp2;
+    int i = 0, n = s.size();
+        
+    for (char c : pattern) {
+        if (i >= n)
+            return false;
+
+        string word = "";
+
+        while (i < n && s[i] != ' ') {
+            word += s[i++];
+        }
+        if (mp.count(c) && mp[c] != word)
+            return false;
+        if (mp2.count(word) && mp2[word] != c)
+            return false;
+
+        mp[c] = word;
+        mp2[word] = c;
+        i++;
+    }
+    return i >= n;
+}
+int main(){
+    string pattern = "abba";
+    string s = "dog cat cat dog";
+    cout<<wordPattern(pattern, s);
+    return 0;
+}
