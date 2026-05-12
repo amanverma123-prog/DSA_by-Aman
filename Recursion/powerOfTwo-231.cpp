0b480e4fe7a2eca4a7e28805dbe8ae3c6a4c984c
Aman Verma <amanverma3911@gmail.com>
2026-05-12 18:18:04 +0530

    Updated LC 231: Power of Two

--- Recursion/powerOfTwo-231.cpp
+++ Recursion/powerOfTwo-231.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// TC = O(n), SC = O(n)
+// Using Recursion TC = O(logn), SC = O(logn)
+// bool isPowerOfTwo(int n) {
+//     if(n == 1) return true;
+//     if(n <= 0 || n%2 != 0) return false;
+//     return isPowerOfTwo(n/2);
+// }
+// int main(){
+//     int n = 256;
+//     cout<<isPowerOfTwo(n);
+//     return 0;
+// }
+
+
+// Optimal without Recursion --> TC = O(1), SC = O(1)
 bool isPowerOfTwo(int n) {
-    if(n == 1) return true;
-    if(n <= 0 || n%2 != 0) return false;
-    return isPowerOfTwo(n/2);
+    return n > 0 && (n & (n-1)) == 0;
 }
 int main(){
     int n = 256;
