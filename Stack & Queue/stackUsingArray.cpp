a3f0991113716e6c06e9c937747821afeed11374
amanverma123-prog <amanverma3911@gmail.com>
2026-07-01 15:58:22 +0530

    Add Stack and Queue folder and implement Stack using Array

---
+++ Stack & Queue/stackUsingArray.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+const int size = 10;
+
+int st[size];
+int topIdx = -1;
+
+void push(int x) {
+    if(topIdx == 9){
+        cout<<"Array is full\n";
+        return;
+    }
+    topIdx += 1;
+    st[topIdx] = x;
+}
+    
+int pop() {
+    if(topIdx == -1){
+        return -1;
+    }
+    return st[topIdx--];
+}
+    
+int top() {
+    if(topIdx == -1){
+        return -1;
+    }
+    return st[topIdx];
+}
+    
+bool isEmpty() {
+    return topIdx == -1;
+}
+
+void display(){
+    if(isEmpty()){
+        cout<<"Stack is Empty\n";
+        return;
+    }
+    cout<<"Stack: ";
+    for(int i = 0;i<=topIdx;i++){
+        cout<<st[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    push(10);
+    push(20);
+    display();
+
+    cout<<"Top: "<<top()<<endl;
+    cout<<"Popped: "<<pop()<<endl;
+
+    push(70);
+
+    display();
+
+    return 0;
+}
