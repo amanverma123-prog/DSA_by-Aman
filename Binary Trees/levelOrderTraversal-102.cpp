3a9a498c7a50fe4199458dc0219f13cfc1a533c1
amanverma123-prog <amanverma3911@gmail.com>
2026-07-30 20:17:59 +0530

    Implementing LC 102: Binary Tree Level Order Traversal

---
+++ Binary Trees/levelOrderTraversal-102.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+struct TreeNode{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    if (root == NULL)
+        return ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int size = q.size();
+        vector<int> level;
+        for (int i = 0; i < size; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node->left != NULL)
+                q.push(node->left);
+            if (node->right != NULL)
+                q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+        }
+    return ans;
+}
+int main(){
+    // Construct the tree
+        //
+        //         1
+        //       /   \
+        //      2     3
+        //     / \     \
+        //    4   5     8
+        //       / \   /
+        //      6   7 9
+    TreeNode* root = new TreeNode(1);
+
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    
+    root->left->right->left = new TreeNode(6);
+    root->left->right->right = new TreeNode(7);
+
+    root->right->right = new TreeNode(8);
+    root->right->right->left = new TreeNode(9);
+
+    vector<vector<int>> levelOrderTraversal = levelOrder(root);
+
+    for (const auto &level : levelOrderTraversal) {
+    for (int node : level) {
+        cout << node << " ";
+    }
+    cout << endl;
+}
+    return 0;
+}
