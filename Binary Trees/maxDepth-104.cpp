64c0be829ea70ce6223a0a74029408109301b37b
amanverma123-prog <amanverma3911@gmail.com>
2026-08-05 20:07:36 +0530

    Implementing LC 104: Maximum Depth of Binary Tree

---
+++ Binary Trees/maxDepth-104.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+struct TreeNode{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// TC = O(n), SC = O(n)
+int maxDepth(TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int lh = maxDepth(root->left);
+    int rh = maxDepth(root->right);
+
+    return 1 + max(lh, rh);
+}
+int main(){
+    // Construct the tree
+        //
+        //         1
+        //       /   \
+        //      2     3
+        //     / \     \
+        //    4   5     8
+        //       / \   /
+        //      6   7 9
+    TreeNode* root = new TreeNode(1);
+
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    
+    root->left->right->left = new TreeNode(6);
+    root->left->right->right = new TreeNode(7);
+
+    root->right->right = new TreeNode(8);
+    root->right->right->left = new TreeNode(9);
+
+    cout<<maxDepth(root);
+    return 0;
+}
